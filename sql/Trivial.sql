fbe7766c5b439ae2cb9b3a3b50078843bfb260fa
Xavi <xavicaromiro@gmail.com>
2017-12-15 17:26:26 +0100

    more quizzes

--- sql/Trivial.sql
+++ sql/Trivial.sql
@@ -17,7 +17,7 @@ CREATE TABLE questions_answers
 (
 	id_qa MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	description VARCHAR(60) NOT NULL,
-	value BIT NULL,
+	value TINYINT(1)  NULL,
 	id_b MEDIUMINT UNSIGNED NULL,
 	id_quiz MEDIUMINT UNSIGNED NOT NULL,
 	CONSTRAINT FK_father FOREIGN KEY (id_b) REFERENCES questions_answers(id_qa),
@@ -36,13 +36,11 @@ CREATE TABLE users
 -- Inserts
 
 -- Users
-
- INSERT INTO users (nick,pass,email) VALUES ("root","root","root@trivial.com");
-
+INSERT INTO users (nick,pass,email) VALUES ("root","root","root@trivial.com");
 -- Quizzes
-
- INSERT INTO quizzes (title,description,theme) VALUES ("Quiz de videojuegos","Este quiz tiene varias preguntas sobre videojuegos de todos los géneros. ¿Te atreves?","Videojuegos");
-
+INSERT INTO quizzes (title,description,theme) VALUES
+("Quiz de videojuegos","Este quiz tiene varias preguntas sobre videojuegos de todos los géneros. ¿Te atreves?","Videojuegos"),
+("Quiz de memes","Este quiz tiene varias preguntas sobre memes en general. REEEEEEE","Memes");
 -- Questions
 -- Videogames
 -- 1
@@ -95,4 +93,51 @@ INSERT INTO questions_answers(id_qa,Description,Value,id_b, id_quiz) VALUES
 (37,"¿En que año salio Counter Striker?",null,null,1),
 (38,"1999.",1,37,1),
 (39,"2000.",0,37,1),
-(40,"2004.",0,37,1)
+(40,"2004.",0,37,1),
+
+-- Memes
+-- 1
+(50,"Smile, Sweet, Sister, Sadistic, Suprise, ...",null,null,2),
+(51,"Service",1,50,2),
+(52,"Spicy",0,50,2),
+(53,"Super.",1,50,2),
+-- 2
+(54,"Somebody ...",null,null,2),
+(55,"is here!",0,54,2),
+(56,"likes you",0,54,2),
+(57,"once told me.",1,54,2),
+-- 3
+(58,"|, |l, ||, |_",null,null,2),
+(59,"get straight",0,58,2),
+(60,"is this a loss reference?",1,58,2),
+(61,"is this a jojo's reference?",0,58,2),
+-- 4
+(62,"To Be Fair, You Have To Have a Very High IQ to Understand ...",null,null,2),
+(63,"Stranger Things",0,62,2),
+(64,"Rick and Morty",1,62,2),
+(65,"Evangelion",0,62,2),
+-- 5
+(66,"Take off your jacket",null,null,2),
+(67,"Man's no hot",1,66,2),
+(68,"But... here?",0,66,2),
+(69,"What the fuck",0,66,2),
+-- 6
+(70,"Sayori",null,null,2),
+(71,"Hang there",1,70,2),
+(72,"I love you",0,70,2),
+(73,"B...Bakka! >///<",0,70,2),
+-- 7
+(74,"When I say go, be ready to throw... Go!",null,null,2),
+(75,"Okay, now let's punch it.",0,74,2),
+(76,"Just follow my moves, and sneak around.",0,74,2),
+(77,"Thorw it on him not me! Uhg, let's try something else.",1,74,2),
+-- 8
+(78,"I'll have two number 9s ...",null,null,2),
+(79,", a number 9 large, a number 2 with extra dip, a number 3, two number 45s, one with cheese, and a large soda.",0,74,2),
+(80,", a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda.",1,74,2),
+(81,", a number 9 large, a number 4 with extra dip, a number 5, two number 45s, one with cheese, and a large soda.",0,74,2),
+-- 9
+(82,"When I say go, be ready to throw... Go!",null,null,2),
+(83,"Okay, now let's punch it.",0,82,2),
+(84,"Just follow my moves, and sneak around.",0,82,2),
+(85,"Thorw it on him not me! Uhg, let's try something else.",1,82,2)
