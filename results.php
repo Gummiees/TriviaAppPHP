ca3b7c0a8b12f66a74a1c2d691d046e293591ef0
Xavi <xavicaromiro@gmail.com>
2017-12-20 20:56:33 +0100

    repared

--- results.php
+++ results.php
@@ -1,8 +1,6 @@
 <?php
 include("includes/header.html");
 include('includes/print_messages.php');
-// check if it is logged in
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['qid']) && isset($_POST['respuestas'])) {
 	require ('mysqli_connect.php');
 
@@ -28,14 +26,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['qid']) && isset($_POST[
 <hr>
 
 <?php
-
 	$q = "SELECT Qt.description, Qt.id_question FROM questions AS Qt INNER JOIN quizzes AS Qz
 	ON Qz.id_quiz = Qt.id_quiz WHERE Qz.id_quiz=$qid";
 	$r = @mysqli_query ($dbc, $q);
 	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
 		$questions[$row['id_question']] = $row['description'];
 	}
-
 	$incorrectas = 0;
 	$preg = 0;
 	foreach ($questions as $key => $question) {
@@ -55,29 +51,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_GET['qid']) && isset($_POST[
 	foreach ($answers as $answer => $value) {
 		if (0 == $value && $res_usr[$preg] == $answer) {
 		 echo '<div class="row respuesta-incorrecta"><div class="col-1 text-center"><i class="fa fa-times" aria-hidden="true"></i></div>';
-		 $incorrectas++;
 		} else if (1 == $value) {
 			echo '<div class="row respuesta-correcta"><div class="col-1 text-center"><i class="fa fa-check" aria-hidden="true"></i></div>';
+			$incorrectas++;
 		} else {
 			echo '<div class="row"><div class="col-1 text-center"><i class="fa fa-circle-o" aria-hidden="true"></i></div>';
 		}
 ?>
 				<div class="col-11 text-left respuesta"><?php echo $answer;?></div>
 			</div>
+			<?php } ?>
 	</div>
 </div>
 
 <?php
-			$preg++;
-		}
-	}
-	$media = $incorrectas/count($questions); // entre 0 y 1
+	$preg++;}
+	$media = 1 - ($incorrectas/count($questions)); // entre 0 y 1
+	echo "$incorrectas / ".count($questions)." = $media";
 	$q = "INSERT INTO stadistics_user (id_quiz, average) VALUES ($qid, $media)";
 	$r = @mysqli_query ($dbc, $q);
-	if (mysqli_num_rows($r) != 1) echo print_message('danger', 'There was an error due to our system.');
 	$q = "INSERT INTO stadistics_quiz (id_user, average) VALUES ($uid, $media)";
 	$r = @mysqli_query ($dbc, $q);
-	if (mysqli_num_rows($r) != 1) echo print_message('danger', 'There was an error due to our system.');
 } else echo print_message('danger', 'You cannot access results unless you finished a quiz.');
 include("includes/footer.html");
 ?>
