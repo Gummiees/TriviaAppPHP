727fe2ca47fb60804ea130d146dc7999ebd158f6
Xavi <xavicaromiro@gmail.com>
2017-12-21 20:47:05 +0100

    semi done

--- user_stats.php
+++ user_stats.php
@@ -2,34 +2,68 @@
 include("includes/header.html");
 include('includes/print_messages.php');
 if (isset($_GET['uid']) && is_numeric($_GET['uid'])) {
+
 	require ('mysqli_connect.php');
 	$uid = $_GET['uid'];
-	$user_stats = array('id_quizzes' => [], 'averages' => [], 'dates' => []);
 
-	$q = "SELECT id_quiz, average, date FROM statistics WHERE id_user=$uid";
+	$q = "SELECT nick FROM users WHERE id_user=$uid";
 	$r = @mysqli_query ($dbc, $q);
-	$num = mysqli_num_rows($r);
-	if ($num > 0) {
-		while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-			$user_stats ['id_quizzes'] [] = $row['id_quiz'];
-			$user_stats ['averages'] [] = $row['average'];
-			$user_stats ['dates'] [] = $row['date'];
-		}
 
-		$last_id=-1;
-		foreach ($user_stats['id_quizzes'] as $id_quiz) {
-			if ($last_id != $id_quiz) {
-				$q = "SELECT title date FROM quizzes WHERE id_quiz=$id_quiz";
-				var_dump($id_quiz);
-				$r = @mysqli_query ($dbc, $q);
-				if (mysqli_num_rows($r) > 0) {
-					while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-						$quiz_titles [] = $row['title'];
-					}
-				} else echo print_message('danger', 'No quiz found.');
-				$last_id = $id_quiz;
+	if (mysqli_num_rows($r) == 1) {
+
+		$row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+		$nick = $row['nick'];
+
+		$user_stats = array('id_quizzes' => [], 'averages' => [], 'dates' => []);
+
+		$q = "SELECT id_quiz, average, date FROM statistics WHERE id_user=$uid";
+		$r = @mysqli_query ($dbc, $q);
+		$num = mysqli_num_rows($r);
+		if ($num > 0) {
+			while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+				$user_stats ['id_quizzes'] [] = intval($row['id_quiz']);
+				$user_stats ['averages'] [] = floatval($row['average']);
+				$user_stats ['dates'] [] = $row['date'];
 			}
-		}
+
+			$last_id=-1;
+			foreach ($user_stats['id_quizzes'] as $key => $id_quiz) {
+				if ($last_id != $id_quiz) {
+					$suma_avg[$id_quiz] = 0;
+					$total_times[$id_quiz] = 0;
+					$q = "SELECT title FROM quizzes WHERE id_quiz=$id_quiz";
+					$r = @mysqli_query ($dbc, $q);
+					if (mysqli_num_rows($r) > 0) {
+						while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+							$quiz_titles [] = $row['title'];
+						}
+					} else echo print_message('danger', 'No quiz found.');
+					$last_id = $id_quiz;
+				}
+				$suma_avg[$id_quiz] += $user_stats['averages'][$key];
+				$total_times[$id_quiz]++;
+			}
+
+			foreach ($suma_avg as $key => $value) {
+				$suma_avg[$key] = round(($value/$total_times[$key])*100, 2);
+			}
+
+			//semana pasada: strtotime('-1 week');
+			$oneweek = date('Y-m-d H:i:s', strtotime('-1 week -1 day'));
+			$yesterday = date('Y-m-d H:i:s', strtotime('-1 day'));
+			var_dump($last_date);
+			$q = "SELECT average, date FROM statistics WHERE (date BETWEEN $oneweek AND $yesterday) AND id_user=$uid";
+			$r = @mysqli_query ($dbc, $q);
+			$num = mysqli_num_rows($r);
+			if ($num > 0) {
+				while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+					$average [] = $row['average'];
+					$date [] = $row['date'];
+					// usar date para sacar la media diara y el total de partidas jugadas
+				}
+
+			}
+
 ?>
 <script src="includes/utils.js"></script>
 <div style="width: 80%; margin-left: 10%;">
@@ -38,7 +72,15 @@ if (isset($_GET['uid']) && is_numeric($_GET['uid'])) {
 <div style="width:80%; margin-left: 10%;">
   <canvas id="canvas2"></canvas>
 </div>
+<div style="width:80%; margin-left: 10%;">
+  <canvas id="canvas3"></canvas>
+</div>
 <script>
+var last7days = [];
+for (var i=7; i>=1; i--) {
+	last7days.push(moment().subtract(i, 'days').format('dddd'));
+}
+
 var randomScalingFactor = function() {
   return Math.round(Math.random() * 100);
 };
@@ -48,26 +90,27 @@ var config1 = {
   type: 'radar',
   data: {
     labels:<?php
-    	echo "['";
+    	echo "[";
     	foreach ($quiz_titles as $key => $quiz_title) {
-    		echo $quiz_title;
-    		if ($key != count($quiz_title)) echo ",";
+    		echo "'$quiz_title'";
+    		if ($key != count($quiz_titles)) echo ",";
     	}
-    	echo "']";
+    	echo "]";
     ?>,
     datasets: [{
-      label: "User",
+      label: "<?php echo $nick;?>",
       backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
       borderColor: window.chartColors.red,
       pointBackgroundColor: window.chartColors.red,
       data: [
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor(),
-        randomScalingFactor()
+      <?php
+      foreach ($suma_avg as $key => $value) {
+      	echo $value;
+      	if ($key != count($suma_avg)) echo ',';
+      }
+      ?>
       ]
-    }, {
+    }/*, {
       label: "My friend",
       backgroundColor: color(window.chartColors.orange).alpha(0.2).rgbString(),
       borderColor: window.chartColors.orange,
@@ -79,7 +122,7 @@ var config1 = {
         randomScalingFactor(),
         randomScalingFactor()
       ]
-    }, {
+    }*/, {
       label: "All user average",
       backgroundColor: color(window.chartColors.blue).alpha(0.2).rgbString(),
       borderColor: window.chartColors.blue,
@@ -111,9 +154,9 @@ var config1 = {
 var config2 = {
   type: 'line',
 	data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: last7days,
     datasets: [{
-      label: "Me",
+      label: "<?php echo $nick;?>",
       backgroundColor: window.chartColors.red,
       borderColor: window.chartColors.red,
       data: [
@@ -126,7 +169,7 @@ var config2 = {
         randomScalingFactor()
       ],
       fill: false,
-  	},{
+  	},/*{
       label: "My friend",
       backgroundColor: window.chartColors.orange,
       borderColor: window.chartColors.orange,
@@ -140,7 +183,7 @@ var config2 = {
         randomScalingFactor()
       ],
       fill: false,
-  	}, {
+  	}, */{
       label: "Users average",
       fill: false,
       backgroundColor: window.chartColors.blue,
@@ -160,7 +203,7 @@ var config2 = {
     responsive: true,
     title:{
       display:true,
-      text:'Total quizzes done per month'
+      text:'Total quizzes done per day last week'
     },
     tooltips: {
       mode: 'index',
@@ -175,14 +218,94 @@ var config2 = {
         display: true,
         scaleLabel: {
           display: true,
-          labelString: 'Month'
+          labelString: 'Day'
         }
       }],
       yAxes: [{
         display: true,
         scaleLabel: {
           display: true,
-          labelString: 'Value'
+          labelString: 'Total quizzes'
+        }
+      }]
+    }
+  }
+};
+var config3 = {
+  type: 'line',
+	data: {
+    labels: last7days,
+    datasets: [{
+      label: "<?php echo $nick;?>",
+      backgroundColor: window.chartColors.red,
+      borderColor: window.chartColors.red,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ],
+      fill: false,
+  	},/*{
+      label: "My friend",
+      backgroundColor: window.chartColors.orange,
+      borderColor: window.chartColors.orange,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ],
+      fill: false,
+  	}, */{
+      label: "Users average",
+      fill: false,
+      backgroundColor: window.chartColors.blue,
+      borderColor: window.chartColors.blue,
+      data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+      ],
+    }]
+  },
+  options: {
+    responsive: true,
+    title:{
+      display:true,
+      text:'Average on quizzes per day last week'
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Day'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Average'
         }
       }]
     }
@@ -191,11 +314,15 @@ var config2 = {
 
 window.onload = function() {
   window.myRadar = new Chart(document.getElementById("canvas1"), config1);
-  window.myLine = new Chart(document.getElementById("canvas2").getContext("2d"), config2);
+  window.myLine1 = new Chart(document.getElementById("canvas2").getContext("2d"), config2);
+  window.myLine2 = new Chart(document.getElementById("canvas3").getContext("2d"), config3);
+  console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 };
+
 </script>
 
 <?php
+			} else echo print_message('danger', 'The user does not exist.');
 		} else echo print_message('danger', 'The user does not have any stats.');
 	} else echo print_message('danger', 'No user selected.');
 include("includes/footer.html");
