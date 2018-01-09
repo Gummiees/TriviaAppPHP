d6ad36641937c01e12ff1e74048abbb644667066
Salva <tecnosalva2011@gmail.com>
2018-01-09 21:27:52 +0100

    index done

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 $page_title = 'Welcome to ATriviaT!';
 session_start();
+require ('mysqli_connect.php');
 include ("includes/header.html");
 include ("includes/content.html");
 include ("includes/footer.html");
--- sql/Trivial.sql
+++ sql/Trivial.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE Trivial;
 
 -- Create table 
-
+USE Trivial;
 -- Create table Users
 CREATE TABLE users
 (
