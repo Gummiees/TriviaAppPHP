23e6910b5bfd6b83a93c6f14259d6201560b1aad
Andreu <andreullabresb@gmail.com>
2018-01-21 05:21:25 +0100

    Manual new user

---
+++ new_user_manual.php
@@ -0,0 +1,38 @@
+<?php 
+session_start();
+include ("includes/header.html");
+?>
+<div align="center">
+	<h2>Are you a new user?</h2><br>
+	<h4>1. Register as a new user</h4><br>
+	<p>First click on the top of the menu in Sign in or Sign Up.</p>
+	<img src="includes/img/cap.png"><br>
+	<p>Next, a page will open with a form, mark the Sign Up tab, which is above the form, fill in the fields and press the button.</p>
+	<img src="includes/img/cap1.png" width="80%" height="400"><br>
+	<p>Fill in the fields in Sign in and press the button to access your account.</p>
+	<img src="includes/img/cap2.png"><br>
+	<p>Once done Log in to the application menu in the upper right part will come with your username.</p>
+	<br><h4>2. What functions does it bring?</h4><br>
+	<p>In the menu there are different sections (1) Statistics, (2) Register quiz, (3) the quiz search engine and finally in the home page the existing quizzes appear.</p>
+	<img src="includes/img/cap3.png" width="80%" height="300">
+	<br><h5>2.1 Open quiz</h5><br>
+	<p>If you press any of the quiz titles for example in videogames (4), start the quiz.</p>
+	<img src="includes/img/cap4.png"><br>
+	<p>You will have to mark one of the three options presented and then click on the next button to advance to the next question.</p>
+	<br><p>When you have just answered all the questions, you will leave all the questions with your answers in view that are correct or with an x to mark incorrect and depending on that the correct ones will be marked.</p>
+	<img src="includes/img/captura6.png"><br>
+	<p>When you finish seeing the correction, press the See my statistics button and various statistics of your user will appear.</p>
+	<img src="includes/img/captura7.png"><br><br>
+	<p>In the search engine you put the name or a few consecutive letters that form almost the title of the quiz, in the case of example: vi</p>
+	<img src="includes/img/captura8.png"  width="80%"><br>
+	<br><h5>2.2 Create a Quiz</h5><br>
+	<p>We go to the home section of the web and click on Register quiz, fill in each section and press the button below to register the quiz.</p>
+	<img src="includes/img/captura9.png"><br>
+	<p>After completing the quiz creation form and pressing the button, generate a message affirming such action.</p>
+	<img src="includes/img/captura10.png"><br>
+	<p>To check that it worked, you have to go to the home page or if you put in the search bar the name of the quiz or a portion of its name.</p>
+	<img src="includes/img/captura11.png"><br>
+</div>
+<?php
+include ("includes/footer.html");
+?>
